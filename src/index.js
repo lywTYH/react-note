3417fa2f8ad58a70dee3123984b0f78c57e8a2e7
lywTYH <763613923@qq.com>
2018-09-27 18:29:55 +0800

    chore: complete the game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import NameForm from './form';
-import Calculator from './calculator';
+// import Calculator from './calculator';
+import Board from './game';
 
 function App() {
-  return <Calculator />;
+  return <Board />;
 }
 ReactDOM.render(<App />, document.getElementById('root'));
